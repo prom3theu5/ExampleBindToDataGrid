03bba5e307c81b8a8112255a048eb23cfae22df4
David Sekula <dave.sekula@gmail.com>
2017-07-12 16:29:25 +0100

    Added Example

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.usersGrid = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.usersGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.usersGrid);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Size = new System.Drawing.Size(384, 615);
+            this.splitContainer1.SplitterDistance = 550;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // usersGrid
+            // 
+            this.usersGrid.AllowUserToAddRows = false;
+            this.usersGrid.AllowUserToDeleteRows = false;
+            this.usersGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.usersGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.usersGrid.Location = new System.Drawing.Point(0, 0);
+            this.usersGrid.Name = "usersGrid";
+            this.usersGrid.ReadOnly = true;
+            this.usersGrid.Size = new System.Drawing.Size(384, 550);
+            this.usersGrid.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(0, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(381, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GET VIEWERS";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 615);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.usersGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView usersGrid;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using TwitchLib;
+using TwitchLib.Models.API.Undocumented.Chatters;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private ConcurrentBag<ChatterFormatted> _users;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // our twitch client id.
+            TwitchAPI.Settings.ClientId = ""; // add your Twitch Client Id.
+            _users = new ConcurrentBag<ChatterFormatted>();
+        }
+
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            // Get Chatters from Api
+            var chatters = await TwitchAPI.Undocumented.GetChatters("burkeblack");
+
+            //If Chatters request was OK - load into concurrentbag and bind to grid.
+            if (chatters is List<ChatterFormatted>)
+            {
+                var newBag = new ConcurrentBag<ChatterFormatted>();
+                // Replaces old bag with new bag - thread safe.
+                Interlocked.Exchange(ref _users, newBag);
+                //add chatters from the response list object to the newbag.
+                foreach (var user in chatters)
+                {
+                    _users.Add(user);
+                }
+                BindGrid();
+            }
+        }
+
+        private void BindGrid()
+        {
+            //clear columns if you are rebinding
+            if (usersGrid.ColumnCount > 0)
+            {
+                usersGrid.Columns.Clear();
+            }
+
+            //stop it making columns
+            usersGrid.AutoGenerateColumns = false;
+
+            //create the username column programatically
+            DataGridViewCell cell = new DataGridViewTextBoxCell();
+            DataGridViewTextBoxColumn colUsername = new DataGridViewTextBoxColumn()
+            {
+                CellTemplate = cell,
+                AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill,
+                Name = "Username",
+                HeaderText = "Username",
+                DataPropertyName = "Username" // Tell the column which property of ChatterFormatted it should use
+            };
+
+            usersGrid.Columns.Add(colUsername);
+
+            var bindingList = new BindingList<ChatterFormatted>(_users.ToList()); // <-- BindingList
+            usersGrid.DataSource = bindingList; // Bind it to the grid.
+}
+    }
+}
